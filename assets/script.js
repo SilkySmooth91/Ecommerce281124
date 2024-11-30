5b838f0f7efc7f2a0025d6ddd3344b1a257f19f0
Lorenzo Olivieri <lorenzo.olivieri13@gmail.com>
2024-11-30 16:09:49 +0100

    es 10-11

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,29 @@ function switchAdress() {
     let paese = document.querySelector("#paese")
     paese.innerText = "Deutchsland"
 }
+
+function addClass() {
+    let linkAmazon = document.querySelectorAll("a")
+    let link
+    for (i = 0; i < linkAmazon.length; i++) {
+        link += linkAmazon[i].classList.add("linkAmazon")
+    }
+    
+}
+
+function changeColor() {
+    let linkAmazon = document.querySelectorAll(".linkAmazon")
+    let linkRossi 
+    for (i = 0; i < linkAmazon.length; i++) {
+        linkRossi += linkAmazon[i].style.color = "red"
+    }
+}
+
+function hideImg() {
+    let img = document.querySelectorAll(".imgProdotto")
+    let imgNascosta
+    for (i = 0; i < img.length; i++) {
+        imgNascosta += img[i].style.display = "none"
+    }
+}
+
